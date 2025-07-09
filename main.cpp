b506d613e581bd5d1e3981764ab787e6d00cb269
Nico Schumann <nico.schumann@startmail.com>
2025-07-09 08:00:05 +0200

    Refactored main.cpp

--- main.cpp
+++ main.cpp
@@ -1,93 +1,28 @@
-// Projekt: Simuliertes Digitales Dharma
-// Proposal: Observing emergently ethic at agent-based systems
-
-#include "environment.hpp"
-#include "kaelon.hpp"
-#include <iostream>
-#include <vector>
-#include <string>
-#include <utility>
+// -- main.cpp --
+#include "Environment.hpp"
+#include "KaelonObserver.hpp"
+#include "StrategyCLI.hpp"
 #include <thread>
-#include <chrono>
-
-bool running = true;
-
-void  evaluateStrategies(std::vector<std::string>& inputs, std::vector<Strategy>&agents) {
-
-    for (std::string& str: inputs){
-        switch(str[0]) {
-            case 'n':
-            case 'N':
-            {
-                agents.push_back(Strategy::NICE);
-                break;
-            }
-            case 'r':
-            case 'R':
-            {
-                agents.push_back(Strategy::RUDE);
-                break;
-            }
-            case 't':
-            case 'T':
-            {
-                agents.push_back(Strategy::TITFORTAT);
-                break;
-            }
-            case 'x':
-            case 'X':
-            {
-                agents.push_back(Strategy::RANDOM);
-                break;
-            }
-            case 'l':
-            case 'L':
-            {
-                agents.push_back(Strategy::LAST10);
-                break;
-            }
-            default:
-            {
-                agents.push_back(Strategy::RUDE);
-            }
-        }
-    }
-}
+#include <iostream>
 
 int main() {
+    StrategyCLI::registerBuiltins();
 
-    // We could redirect the input stream from a file
-    std::vector<std::string> input;
-    for (std::string tmp; std::cin >> tmp; input.push_back(tmp)) {};
-    std::vector<Strategy> agents;
-    evaluateStrategies(input, agents);
+    Environment env(10);
+    KaelonObserver observer;
+    env.attach(&observer);
 
-    Environment env(agents);
-
-    // It's a lambda fuction (closure)
-    std::thread kaelonThread([&env](){
-        while (running) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            auto stats = env.snapshot();
-            Kaelon::analyze(stats);
-        }
+    std::thread envThread([&env]() {
+        env.run(50); // run for 50 rounds
     });
 
-    env.showStats();
-    std::cout << "\n====================\n";
+    std::thread cliThread([]() {
+        StrategyCLI::repl();
+    });
 
-    for (size_t i = 0; i < 10; ++ i) {
-        if (i > 0) {
-            env.survivalOfFittest();
-        }
-        for (size_t j = 0; j < 50; ++j) {
-            env.runCycle();
-        }
-    }
-    env.showStats();
+    envThread.join();
+    cliThread.join();
 
-    running = false;
-    kaelonThread.join();
-    
     return 0;
 }
+
